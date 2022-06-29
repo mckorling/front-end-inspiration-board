d278da14cf54d9cb9bf829b232c27cb5684b056d
Diana <arantxa.codes@gmail.com>
2022-06-29 12:05:01 -0700

    rl & dm: added components for board list and added axios call for boards on app

--- src/App.js
+++ src/App.js
@@ -2,8 +2,28 @@ import logo from "./logo.svg";
 import "./App.css";
 import cardsData from "./cardsData.json";
 import CardList from "./components/CardList";
+import axios from "axios";
 
 function App() {
+  const [boardData, setBoardData] = useState([]);
+
+  const getBoards = () => {
+    axios
+      .get("https://team-green-inspo.herokuapp.com/boards")
+      .then((response) => {
+        const newBoards = response.data.map((board) => {
+          return {
+            id: board.id,
+            title: board.title,
+            owner: board.owner,
+          };
+        });
+        setBoardData(newBoards);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
+  };
   return (
     <div>
       <CardList cardsData={cardsData} />
